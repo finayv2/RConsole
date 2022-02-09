44c3e839e3e9a0f0138c1fca4fb151ed61dcf74a
finayv2 <75693103+finayv2@users.noreply.github.com>
2022-02-09 16:18:24 -0500

    Update RConsoleAPI.lua

--- RConsoleAPI.lua
+++ RConsoleAPI.lua
@@ -20,12 +20,30 @@ local CustomRConsole = {
 
 }
 
+local SpecFunctions = {
+    ['Debug'] = function()
+        rconsoleprint("@@RED@@")
+        rconsoleprint('[Debug]')
+    end;
+
+    ["Time"] = function()
+        local timestamp = string.format("%02i:%02i %s", ((hour - 1) % 12) + 1, os.date("!*t").min, (os.date("!*t").hour + 1) % 24 < 12 and "AM" or "PM")
+
+        rconsoleprint("@@WHITE@@")
+        rconsoleprint(tostring(timestamp)..":")
+    end;
+}
+
 for i,v in pairs(CustomRConsole.Colors) do i = string.lower(i) end
 
-function CustomRConsole.PrintText(str, Color)
+function CustomRConsole.PrintText(str, Color, Specs)
     if type(str) ~= 'string' then str = tostring(str) end;
-    if Color ~= nil then rconsoleprint(CustomRConsole.Colors[string.lower(Color)]) end;
 
+    if Specs ~= nil then
+        SpecFunctions[Specs]()
+    end
+
+    if Color ~= nil then rconsoleprint(CustomRConsole.Colors[string.lower(Color)]) end;
     rconsoleprint(str)
 end
 
